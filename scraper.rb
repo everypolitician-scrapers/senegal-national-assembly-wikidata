ba55560ab89b354d0605c9f2c76947fb0fb3a3b4
Tony Bowden <tony@tmtm.com>
2016-02-21 08:07:55 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -1,31 +1,9 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'json'
-require 'pry'
-require 'rest-client'
-require 'scraperwiki'
 require 'wikidata/fetcher'
-require 'mediawiki_api'
 
-def members
-  morph_api_url = 'https://api.morph.io/tmtmtmtm/senegal-national-assembly/data.json'
-  morph_api_key = ENV["MORPH_API_KEY"]
-  result = RestClient.get morph_api_url, params: {
-    key: morph_api_key,
-    query: "select DISTINCT(wikiname) AS wikiname from data"
-  }
-  JSON.parse(result, symbolize_names: true)
-end
-
-WikiData.ids_from_pages('fr', members.map { |c| c[:wikiname] }).each_with_index do |p, i|
-  data = WikiData::Fetcher.new(id: p.last).data('fr') rescue nil
-  unless data
-    warn "No data for #{p}"
-    next
-  end
-  ScraperWiki.save_sqlite([:id], data)
-end
-
-warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
+names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/senegal-national-assembly', column: 'wikiname')
+EveryPolitician::Wikidata.scrape_wikidata(names: { fr: names })
+warn EveryPolitician::Wikidata.notify_rebuilder
 
